0ef724a9e3098c0c0fea17efac1252a06995537b
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-01-28 14:20:31 +0000

    Went back to bardecoder and initialised camera out of the loop to increase speed

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,21 @@
 # a cross-compile toolchain installed, so that's less work for us.
 FROM rustembedded/cross:arm-unknown-linux-gnueabihf-0.2.1
 
+ENV TZ=Europe/London
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 RUN apt-get update
 RUN dpkg --add-architecture armhf && \
     apt-get update && \
-    apt-get install --assume-yes wget
+    apt-get upgrade --assume-yes && \
+    apt-get install --assume-yes wget && \
+    apt-get download libopencv-dev:armhf llvm clang libclang-10-dev:armhf libopencv-calib3d-dev:armhf libopencv-contrib-dev:armhf libopencv-features2d-dev:armhf libopencv-highgui-dev:armhf libopencv-imgcodecs-dev:armhf libopencv-objdetect-dev:armhf libopencv-stitching-dev:armhf libopencv-superres-dev:armhf libopencv-videoio-dev:armhf libopencv-videostab-dev:armhf libopencv-viz-dev:armhf libopencv-calib3d3.2:armhf libopencv-contrib3.2:armhf libopencv-features2d3.2:armhf libopencv-highgui3.2:armhf libopencv-imgcodecs3.2:armhf libopencv-videoio3.2:armhf
 
 RUN wget "http://192.168.0.10:8000/libraspberrypi-dev_1.20210108-1_armhf.deb"
 RUN wget "http://192.168.0.10:8000/libraspberrypi0_1.20210108-1_armhf.deb"
 RUN wget "http://192.168.0.10:8000/raspberrypi-bootloader_1.20210108-1_armhf.deb"
 
-RUN dpkg -i raspberrypi-bootloader_1.20210108-1_armhf.deb
-RUN dpkg -i libraspberrypi0_1.20210108-1_armhf.deb
-RUN dpkg -i libraspberrypi-dev_1.20210108-1_armhf.deb
+# RUN dpkg -i raspberrypi-bootloader_1.20210108-1_armhf.deb
+# RUN dpkg -i libraspberrypi0_1.20210108-1_armhf.deb
+# RUN dpkg -i libraspberrypi-dev_1.20210108-1_armhf.deb 
+RUN dpkg -i  *.deb
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use bardecoder;
 extern crate image;
 use image::{DynamicImage, ImageBuffer};
-use quirc::QrCoder;
 use rascam::*;
 use std::fs::File;
 use std::io::Write;
@@ -16,16 +15,12 @@ fn main() {
         // note that this doesn't run destructors
         ::std::process::exit(1);
     }
-    loop {
-        read_qr_from_camera(&info.cameras[0]);
-    }
-}
 
-fn read_qr_from_camera(info: &CameraInfo) {
-    const WIDTH: u32 = 3280;
-    const HEIGHT: u32 = 2464;
+    // Raspberry Pi HQ Cam resolution / 5
+    const WIDTH: u32 = 811;
+    const HEIGHT: u32 = 608;
 
-    let mut camera = SimpleCamera::new(info.clone()).unwrap();
+    let mut camera = SimpleCamera::new(info.cameras[0].clone()).unwrap();
     let settings = CameraSettings {
         width: WIDTH,
         height: HEIGHT,
@@ -37,31 +32,27 @@ fn read_qr_from_camera(info: &CameraInfo) {
     let sleep_duration = time::Duration::from_millis(2000);
     thread::sleep(sleep_duration);
 
+    loop {
+        let img = camera.take_one().expect("Failed to take image");
+        read_qr_from_camera(img);
+    }
+}
+
+fn read_qr_from_camera(img: Vec<u8>) {
     println!("Taking image");
-    let img = camera.take_one().unwrap();
-    // File::create("image.jpg")
-    //     .unwrap()
-    //     .write_all(&camera.take_one().unwrap())
-    //     .unwrap();
 
     let decoder = bardecoder::default_decoder();
-    // let image_file = image::open("image.jpg").unwrap();
-    let image_file = image::load_from_memory(&img).unwrap().to_luma();
-
-    let mut quirc = QrCoder::new().unwrap();
+    let image_file = image::load_from_memory(&img).unwrap();
 
     println!("Decoding");
-    // let results = decoder.decode(&image_file);
-    let results = quirc.codes(&image_file, WIDTH, HEIGHT).unwrap();
+    let results = decoder.decode(&image_file);
     for result in results {
         match result {
             Ok(msg) => {
-                // println!("{}", msg)
-                println!("{:?}", msg)
+                println!("{}", msg)
             }
             Err(e) => {
-                // println!("{}", e)
-                println!("{:?}", e)
+                println!("{}", e)
             }
         }
     }
