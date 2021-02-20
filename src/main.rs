7a13ebde522c03343c631c9587016eee9041fd68
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-02-20 18:19:07 +0000

    Added initial setup check

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ mod qr_scanner;
 mod structs;
 
 fn main() {
-    qr_scanner::initial_setup();
+    // If the base url isn't set, assume that initial setup hasn't been completed
+    if structs::CameraSoftwareSettings::load_from_confy()
+        .expect("Failed to read config!")
+        .base_url
+        == "NOT_SET"
+    {
+        qr_scanner::initial_setup();
+    }
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,9 +1,35 @@
 use serde::{Deserialize, Serialize};
-use uuid;
+use uuid::Error as UuidError;
+use uuid::Uuid;
 
 /// This struct holds all the camera software settings and is loaded on startup.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct CameraSoftwareSettings {
-    base_url: String,
-    camera_token: uuid::Uuid,
+    pub base_url: String,
+    pub camera_token: Uuid,
+}
+
+impl CameraSoftwareSettings {
+    pub fn from_qr_string(qr_string: String) -> Result<CameraSoftwareSettings, UuidError> {
+        let qr_string_split: Vec<&str> = qr_string.split(",").collect();
+        let camera_token_uuid = Uuid::parse_str(qr_string_split[1])?;
+
+        Ok(CameraSoftwareSettings {
+            base_url: qr_string_split[0].to_string(),
+            camera_token: camera_token_uuid,
+        })
+    }
+
+    pub fn load_from_confy() -> Result<CameraSoftwareSettings, confy::ConfyError> {
+        confy::load("camera-software-new")
+    }
+}
+
+impl Default for CameraSoftwareSettings {
+    fn default() -> Self {
+        CameraSoftwareSettings {
+            base_url: "NOT_SET",
+            camera_token: Uuid::nil(),
+        }
+    }
 }
